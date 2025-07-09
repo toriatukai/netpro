21c390f7418ea77673e1864806dcfbb397a53527
23fi121 <14tomo.sushi@gmail.com>
2025-07-09 12:07:44 +0900

    add_script

--- Assets/Script/Main/ScoreManager.cs
+++ Assets/Script/Main/ScoreManager.cs
@@ -3,16 +3,35 @@ using System.Collections.Generic;
 
 public class ScoreManager : MonoBehaviour
 {
-    private List<float> _scoreList;
+    private List<float> hostHitTimes;
+    private List<float> clientHitTimes;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _scoreList = new List<float>();
+        hostHitTimes = new List<float>();
+        clientHitTimes = new List<float>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void SetHitTimeList(ulong clientId, float hitTime)
+    {
+        // clientId == 0はホスト、それ以降は連番でクライアント
+        // 今回は1 vs 1なのでlistで保存
+        if (clientId == 0)
+            hostHitTimes.Add(hitTime);
+        else
+            clientHitTimes.Add(hitTime);
+    }
+
+    public bool DidPlayerWin()
+    {
+
+        return true;
     }
 }
